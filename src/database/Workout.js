7e41390e89e7c2288c2aacfa8ab380ac0c810625
chiru-hub <56336144+chiru-hub@users.noreply.github.com>
2022-08-21 01:10:12 +0530

    halfway

--- src/database/Workout.js
+++ src/database/Workout.js
@@ -18,11 +18,18 @@ const createNewWorkout = newWorkout => {
     DB.workouts.findIndex(workout => workout.name === newWorkout.name) > -1
 
   if (isAlreadyAdded) {
-    return
+    throw {
+      status: 400,
+      message: `Workout with the name '${newWorkout.name}' already exists`,
+    }
   }
-  DB.workouts.push(newWorkout)
-  saveToDatabase(DB)
-  return newWorkout
+  try {
+    DB.workouts.push(newWorkout)
+    saveToDatabase(DB)
+    return newWorkout
+  } catch (error) {
+    throw { status: 500, message: error?.message || error }
+  }
 }
 
 const updateOneWorkout = (workoutId, changes) => {
@@ -32,6 +39,31 @@ const updateOneWorkout = (workoutId, changes) => {
   if (indexForUpdate === -1) {
     return
   }
+  const updatedWorkout = {
+    ...DB.workouts[indexForUpdate],
+    ...changes,
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+  }
+  DB.workouts[indexForUpdate] = updatedWorkout
+  saveToDatabase(DB)
+  return updatedWorkout
 }
 
-module.exports = { getAllWorkouts, createNewWorkout, getOneWorkout }
+const deleteOneWorkout = workoutId => {
+  const indexForDeletion = DB.workouts.findIndex(
+    workout => workout.id === workoutId
+  )
+  if (indexForDeletion === -1) {
+    return
+  }
+  DB.workouts.splice(indexForDeletion, 1)
+  saveToDatabase(DB)
+}
+
+module.exports = {
+  getAllWorkouts,
+  createNewWorkout,
+  getOneWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+}
