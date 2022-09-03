4f969aed46822c64e047c8e0eac42c9e229ba129
Chiranjib Paul <chiranjibpaul007@gmail.com>
2022-09-03 21:14:12 +0530

    changes

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -18,13 +18,8 @@ const createNewWorkout = newWorkout => {
     createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
     updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
   }
-  try {
-    const createdWorkout = Workout.createNewWorkout(workoutToInsert)
-    return createdWorkout
-  } catch (error) {
-    throw error
-  }
-
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert)
+  return createdWorkout
   // return
 }
 
